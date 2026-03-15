8cdacd53c2e8f5a06942e7160ff747849462fab9
Sai <whyfory91@gmail.com>
2026-03-15 22:18:02 +0530

    Conditions and Scanner

---
+++ src/conditions/PassFail.java
@@ -0,0 +1,14 @@
+package conditions;
+
+import java.util.Scanner;
+
+public class PassFail {
+    public static void main(String[] args){
+        int marks= 5;
+        if(marks>=35){
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
---
+++ src/conditions/PositiveNegative.java
@@ -0,0 +1,12 @@
+package conditions;
+
+public class PositiveNegative {
+    public static void main(String[] args){
+        int a = 5;
+        if (a>=0){
+        System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
